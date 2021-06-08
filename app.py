f7f7d034c25a51de15b232770d01bec37c01d0d9
Shriharan <74127448+tonyshree18@users.noreply.github.com>
2021-06-08 11:32:25 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
-from covid import Covid
-covid = Covid()
-country_name = input('Enter the country name: ')
-cases = covid.get_status_by_country_name(country_name)
+# This code was written by Shreeharan for the YouTube Channel Stark Intelligence
+from covid import Covid     # mporting the modules
+covid = Covid()             # defining the variable covid
+country_name = input('Enter the country name: ')    # geting input from the user
+cases = covid.get_status_by_country_name(country_name)  # getting cases
 for x in cases:
-    print(x,':',cases[x])
+    print(x,':',cases[x])       # printing cases
